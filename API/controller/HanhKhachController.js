8c9378f93a2a6682691cff404938d644ffc9f5be
MinhThinh1111 <minhthinhvo123@gmail.com>
2023-11-13 10:56:12 +0700

    api

---
+++ API/controller/HanhKhachController.js
@@ -0,0 +1,71 @@
+var HanhKhach = require('../model/HanhKhach');
+
+const HanhKhachController = {
+    getAll:(req,res)=>{
+        HanhKhach.getAll((err,data)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(data);
+            }
+        })
+    },
+
+    insert:(req,res)=>{
+        HanhKhach.insert(req.body,(err,data)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(data);
+            }
+        })
+    },
+
+    update:(req,res)=>{
+        HanhKhach.update(req.body,(err,data)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(data);
+            }
+        })
+    },
+
+    delete:(req,res)=>{
+        HanhKhach.delete(req.params.id,(err,data)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(data);
+            }
+        })
+    },
+
+    getById:(req,res)=>{
+        HanhKhach.getById(req.params.id,(err,data)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(data[0]);
+            }
+        })
+    },
+
+    getBySDT:(req,res)=>{
+        HanhKhach.getBySDT(req.params.Sdt,(err,data)=>{
+            if(err){
+                res.json(err);
+            }
+            else{
+                res.json(data);
+            }
+        })
+    }
+}
+
+module.exports = HanhKhachController;
